c23ce624371cc8bfe755a2c4e304844dfafa6e90
luna <rickharrison422@gmail.com>
2024-02-01 12:47:25 -0600

    starting to overhaul portfolio page

--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -3,10 +3,22 @@
 </script>
 
 <Nav />
-<main></main>
+<main>
+  <div class="btns">
+    <a href="#/clock">clock</a>
+  </div>
+</main>
 
 <style>
   main {
     background-image: url("videos/background.jpg");
+    height: 100vh;
+  }
+  .btns {
+    margin-top: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
   }
 </style>
