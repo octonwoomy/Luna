3c877b1d513b3007760a6e8323d5a69f011000c1
luna <rickharrison422@gmail.com>
2024-02-26 12:51:43 -0600

    start of portfolio page, good progress on layout

--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -4,21 +4,56 @@
 
 <Nav />
 <main>
-  <div class="btns">
-    <a href="#/clock">clock</a>
+  <div class="container">
+    <figure>
+      <a href="#/clock"><img src="../videos/image.png" alt="timer" /></a>
+      <figcaption>Simple Timer in C</figcaption>
+    </figure>
+
+    <figure>
+      <a href="#/chatbot"><img src="../videos/chat.png" alt="timer" /></a>
+      <figcaption>JSON chatbot made in python</figcaption>
+    </figure>
+
+    <figure>
+      <a href="#/restapi"><img src="../videos/aou.png" alt="timer" /></a>
+      <figcaption>API demo made with gin and golang</figcaption>
+    </figure>
+
+    <figure>
+      <a href="#/chatbot"><img src="../videos/image.png" alt="timer" /></a>
+      <figcaption>Video compressor made with python</figcaption>
+    </figure>
   </div>
 </main>
 
 <style>
   main {
-    width: 100%;
+    position: relative;
     height: 100vh;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    gap: 20px;
+    justify-content: center;
   }
-  .btns {
-    margin-top: 40px;
+  .container {
     display: flex;
+    flex-wrap: wrap;
     justify-content: center;
     align-items: center;
-    color: white;
+    padding-top: 40px;
+  }
+  figure {
+    max-height: 300px;
+    width: 450px;
+    background-color: #2f2f2f;
+    text-align: center;
+    border: 10px solid #2f2f2f;
+    font-size: 20px;
+  }
+  figure img {
+    width: 450px;
+    height: 250px;
   }
 </style>
